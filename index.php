5223241125421fb298272a74de07b068ac7b4007
Patryk <patrykmigala0@gmail.com>
2025-09-03 21:53:50 +0200

    change layout and change images and header

--- index.php
+++ index.php
@@ -5,26 +5,60 @@
 <!DOCTYPE html>
 <html lang="pl">
     <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=keyboard_double_arrow_down" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta type="description" content="Popularny hotel o różnorodnych pokojach dla każdego" />
         <link rel="stylesheet" href="./styles/globalStyles.css">
+        <link rel="icon" href="./source/logo.svg" />
         <title>Hotel Floria</title>
     </head>
     <body>
         <div id="container">
-            <header>
+            <div id="header-container">
 
+                <!-- <header id="header-in-scroll">
+                    <img src="./source/logo.ico" alt="logo" />
+                    
+                <ul>
+                    <li class="active"><a href="">Hotel</a></li>
+                    <li><a href="">Pokoje</a></li>
+                    <li><a href="">Restauracja</a></li>
+                    <li><a href="">Kontakt</a></li>
+                    <li><a href="">Rezerwuj</a></li>
+                </ul>
+            </header> -->
+            <header id="header-initial">
+                <img src="./source/logo.svg" alt="logo" />
+                <ul>
+                    <li class="active"><a href="">Hotel</a></li>
+                    <li><a href="">Pokoje</a></li>
+                    <li><a href="">Restauracja</a></li>
+                    <li><a href="">Kontakt</a></li>
+                    <li><a href="">Rezerwuj</a></li>
+                </ul>
+                <span><img src="./source/pagesImg/double-arrow-down.svg"></span>
             </header>
+            
+        </div>
+
+            
             <nav>
 
             </nav>
+
             <main>
+                start:
+                <input type="date" id="data" >
+                <br/>
+                end:
+                <input type="date" id="data">
+                <br/>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse dicta voluptatum fugiat alias, impedit ipsa incidunt quam, assumenda qui debitis, nihil sit corporis quae eum velit dolor modi reprehenderit quaerat tenetur vitae ea aut dolores est dolorum? Iusto quam voluptatibus harum veritatis inventore nobis, magnam delectus voluptatum accusamus rem ullam adipisci provident quia veniam doloribus dicta obcaecati minima, quod quasi odio, non iure. Quasi quo sequi optio ab? Alias eos nam minus, reprehenderit dolorum odio aliquid. Animi facere autem laboriosam. Quia, dolorum laboriosam hic quae quasi tempora sapiente quibusdam odit voluptates exercitationem, perferendis necessitatibus vitae. Quam totam unde assumenda corrupti vel rem alias, sequi harum voluptate, libero quod laboriosam veniam sapiente error aperiam. Expedita distinctio hic, totam molestias et perspiciatis quos porro, vitae qui omnis, reiciendis sapiente perferendis. At doloremque vero nobis reiciendis esse vitae tempora neque magni! Modi distinctio, dolorum, aliquid magnam recusandae illum quam, iste eligendi corporis minima necessitatibus eum fuga tenetur similique harum amet reiciendis at voluptates sunt id eos sit. Rerum commodi, temporibus optio voluptatibus laborum minus? Sed vel autem impedit odit dolor a ipsum veritatis assumenda ad architecto ducimus sunt, maxime illum officiis similique fugit laboriosam laborum numquam facere doloremque maiores. Culpa enim alias incidunt excepturi ducimus possimus! Sed sapiente quae praesentium id. Temporibus corrupti accusantium commodi asperiores nostrum quae incidunt amet optio accusamus earum tenetur, consequuntur quo explicabo in porro rerum corporis at ratione repudiandae inventore sequi adipisci quam? Aliquam ipsam eum rem repellendus cumque accusamus, porro pariatur quo nisi tempora, nostrum unde alias ut quia perspiciatis eaque! Autem, fugit voluptatem. Error officiis itaque aliquid laborum autem, nam incidunt, sit magni officia magnam repellat aperiam enim id ducimus, corporis recusandae nemo ex ipsa ullam cum. Optio dolorum ipsum, cumque voluptate dolorem officiis amet quae assumenda fugiat culpa dolore hic ad ea eaque ut praesentium omnis molestiae aliquid aut repellat! Non esse modi veniam quasi amet, magnam nesciunt doloremque mollitia accusantium ratione? Accusantium dolorem quo id minus necessitatibus impedit doloribus corporis rerum pariatur dolores? Eligendi voluptas ratione eos perferendis totam enim omnis voluptatem quaerat vitae. Officia repellat unde ipsam porro quod voluptatem voluptates beatae dolorum ullam omnis ab accusamus cupiditate nulla explicabo impedit reprehenderit et illo quidem, distinctio maxime velit, doloremque minima odio. Incidunt soluta animi consequuntur molestiae suscipit explicabo beatae voluptate vitae odit iure error enim deleniti magnam rerum et architecto, accusantium quia repudiandae. Similique est ipsa odio minus optio! Repellendus beatae ea dignissimos, officia nesciunt animi ullam recusandae doloribus. Doloribus, fugiat ea qui tempora accusantium velit beatae eveniet dolor enim cumque nesciunt magnam sapiente consectetur fuga! Quaerat nostrum cupiditate laboriosam quo necessitatibus dolor repellendus rem libero asperiores? Nisi vero iure unde ratione dolorem temporibus necessitatibus magnam pariatur enim quisquam adipisci similique molestias quis aut eligendi corrupti, doloribus officiis iusto reiciendis animi consequuntur excepturi voluptatem. Hic vel ad omnis eveniet perspiciatis minus at dolores doloremque assumenda, ut molestiae atque dolor rerum incidunt reiciendis dignissimos consequatur cumque quisquam, itaque sit dolorum labore excepturi? Tempore veniam excepturi commodi asperiores sed, nobis expedita reprehenderit, vel dicta temporibus quos, cupiditate sint eius quae explicabo! Nam eveniet laudantium corrupti ratione obcaecati sit tenetur porro consequatur expedita incidunt sint debitis quia corporis laboriosam perferendis saepe, et eos, magnam nihil, veritatis quas. Optio qui at dignissimos tempore fugit voluptates quod excepturi commodi id? Dolore assumenda nulla corrupti repudiandae quisquam nam eum, perspiciatis eaque voluptatum quis, fugit molestias aut libero quaerat quia, est pariatur voluptatem tempora! Nemo praesentium cumque, nam atque dolorem at. Inventore sit ducimus possimus asperiores fugiat nihil nostrum itaque consequatur at tenetur dolores assumenda iste culpa, quod eum facere hic sapiente sed magni, officia quidem molestias? Quisquam veniam perspiciatis a, adipisci quod ad error amet magnam saepe debitis laborum nesciunt modi voluptatibus cum voluptas minima nam perferendis! Dolorem optio, autem aliquam quaerat deleniti repellat sapiente necessitatibus deserunt sed velit doloribus similique officiis consequatur assumenda pariatur culpa excepturi error ipsum veritatis aliquid, nihil amet temporibus cum. Corporis dolorum quibusdam cum nihil eligendi delectus, praesentium, quas distinctio quidem error doloremque eos reprehenderit magnam aspernatur, nemo porro consequatur beatae illo tempora dolores. Excepturi labore tempore reprehenderit totam autem ducimus non provident aliquid! Magni quam quas facilis doloremque nobis voluptate blanditiis rem, reprehenderit vero, mollitia aliquid expedita error beatae id iste explicabo minima commodi dignissimos debitis illum nostrum quis. Asperiores provident praesentium illum iure maxime omnis recusandae libero dolor laboriosam voluptatem amet architecto veniam, accusamus minima? Iure, veritatis. Magnam, laboriosam consequuntur veritatis qui saepe deleniti, aliquam fugit maxime, doloremque ab quod perferendis. Molestiae omnis illum atque eos blanditiis error, aspernatur quis eveniet quaerat, delectus vel exercitationem perspiciatis quisquam esse rem. Iste debitis deleniti doloremque quisquam repellat nihil, nulla quibusdam veniam commodi iure perferendis ex fugiat nemo quae vel consequatur illum? Aliquid quam enim porro necessitatibus molestiae, assumenda ea cum a. Dolore omnis obcaecati distinctio alias dolores quasi modi nostrum numquam dignissimos, incidunt inventore magnam placeat architecto animi cumque labore itaque harum facilis rerum voluptatem veritatis unde aut aliquid culpa? Cum fugit corporis qui porro debitis quisquam quae nulla, vitae nam, enim odio officiis, id libero voluptas! At, hic expedita neque minus dolorum libero beatae molestias eum placeat dolorem. Officiis, odio asperiores neque consequatur illo nostrum explicabo dolore quo molestias facere ullam tempore pariatur, voluptas sit natus itaque ab ducimus provident nesciunt alias? Dolores ipsum atque iure! Aspernatur enim qui illum laboriosam dolorum harum itaque ullam eius? Unde, dolore ipsum alias inventore ipsa deserunt fugiat delectus sunt sed, consequuntur debitis ad suscipit soluta veniam, doloremque sequi sapiente necessitatibus quis ullam minus illo molestias? Rem ipsum odit alias aliquid dolores esse minima incidunt porro dicta excepturi similique dolor et at, repudiandae minus itaque totam voluptatum facilis consequatur dignissimos iusto magnam nesciunt in corrupti? Esse corporis ullam odio officia assumenda quas dolorem quaerat temporibus, debitis optio, ad itaque sint minima dolores sapiente accusantium non quis placeat aspernatur. Magni corporis sit veritatis soluta fuga, nisi asperiores eveniet, magnam nemo laboriosam ipsam harum, labore ut doloribus rerum expedita aspernatur recusandae numquam quae officiis porro repellendus voluptatum cupiditate. Adipisci voluptas provident repudiandae officiis ut maxime. Repudiandae, at quasi. Aliquam?
             </main>
             <footer>
-
             </footer>
         </div>
     </body>
+
 </html>
